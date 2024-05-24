aacd52944526e26a95a51ae31123edf01fcb868c
Nikolina Sviracevic <sviracevicn@gmail.com>
2024-05-24 17:12:01 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,43 +1,63 @@
-import requests
-import json
+from models.genericClient import GenericClient
+from models.board import Board
+from models.list import List
 
-ApiKey="737f990a50b95a1db675188c99175c8a"
-Token="ATTA7d15664b7f73521ad1fafa12717b24d6de9136f780224304473d52fe59b0452e0935F6AF"
-list_id="66460051007109943d036a44"
-board_id="6645fee8d15bb6bc3076c8e9"
-urlPost="https://api.trello.com/1/cards"
-urlGet=f'https://api.trello.com/1/boards/{board_id}/cards'
-
-query = {
-    'key': ApiKey,
-    'token': Token
-}
-
-response = requests.get(urlGet, params=query)
-
-if response.status_code == 200:
-
-    cards = response.json()
-
-    for card in cards:
-        print(f"Card Name: {card['name']}")
-        print(f"Card ID: {card['id']}")
-        print(f"Card Description: {card['desc']}")
-        print(f"Card URL: {card['shortUrl']}")
-        print("------")
-else:
-    print(f"Neuspesno: {response.status_code}")
-    print(response.text)
+client = GenericClient("737f990a50b95a1db675188c99175c8a", "ATTA7d15664b7f73521ad1fafa12717b24d6de9136f780224304473d52fe59b0452e0935F6AF", "https://api.trello.com/1")
 
 
-def Napravi_Karticu(ime,desc):
+fetched_board_data = client.get("boards/6645fee8d15bb6bc3076c8e9")
+board = Board(fetched_board_data.get("id"),fetched_board_data.get("name"),fetched_board_data.get("desc"),fetched_board_data.get("shortUrl"))
+print(board)
 
-    queryPost = {"key":ApiKey,"token":Token,"name":ime,"desc":desc,"idList":list_id}
-
-    nova_karta = requests.post(urlPost,queryPost)
-    print("Uspesno napravljena nova kartica!")
+fetched_list_data = client.get("lists/66460051007109943d036a44")
+trello_list = List(fetched_list_data.get("id"),fetched_list_data.get("name"),fetched_list_data.get("idboard"))
+print(trello_list)
 
 
 
-Napravi_Karticu("TestKartica NOVA","TestDescription NOVA")
 
+
+
+
+#prvi zadatak
+# import requests
+
+# api_key = "737f990a50b95a1db675188c99175c8a"
+# token = "ATTA7d15664b7f73521ad1fafa12717b24d6de9136f780224304473d52fe59b0452e0935F6AF"
+# list_id = "66460051007109943d036a44"
+# board_id = "6645fee8d15bb6bc3076c8e9"
+# url_post = "https://api.trello.com/1/cards"
+# url_get = f'https://api.trello.com/1/boards/{board_id}/cards'
+
+# def ispisi_kartice(url):
+#     query = {
+#         'key': api_key,
+#         'token': token
+#     }
+
+#     response = requests.get(url, params=query,timeout=10)
+
+#     if response.status_code == 200:
+#         cards = response.json()
+#         for card in cards:
+#             print(f"Card Name: {card['name']}")
+#             print(f"Card ID: {card['id']}")
+#             print(f"Card Description: {card['desc']}")
+#             print(f"Card URL: {card['shortUrl']}")
+#             print("------")
+#     else:
+#         print(f"Neuspesno: {response.status_code}")
+#         print(response.text)
+
+# def napravi_karticu(ime,desc):
+
+#     query_post = {"key":api_key,"token":token,"name":ime,"desc":desc,"idList":list_id}
+
+#     response = requests.post(url_post,query_post,timeout=10)
+#     if response.status_code == 200:
+#         print("Uspesno napravljena nova kartica!")
+
+
+
+# napravi_karticu("TestKartica NOVA","TestDescription NOVA")
+# ispisi_kartice(url_get)
---
+++ models/list.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+@dataclass
+class List:
+    id: str
+    name: str
+    idBoard: str
