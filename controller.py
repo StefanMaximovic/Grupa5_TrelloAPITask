ac34aba5cc7c380458de13267fb4ddd552e13f65
StefanMaksimovic <stefanmaksimovic1501@gmail.com>
2024-05-30 13:56:17 +0200

    Dodat controller.py za Fast Api

---
+++ controller.py
@@ -0,0 +1,71 @@
+from fastapi import FastAPI
+from genericClient import GenericClient
+from models.board import Board
+from models.list import List
+from models.card import Card
+from models.checkList import CheckList
+from models.comment import Comment
+from fileManager import FileManager
+import requests
+
+
+app = FastAPI()
+client = GenericClient("737f990a50b95a1db675188c99175c8a", "ATTA7d15664b7f73521ad1fafa12717b24d6de9136f780224304473d52fe59b0452e0935F6AF", "https://api.trello.com/1")
+
+
+#127.0.0.1:8000/boards/6645fee8d15bb6bc3076c8e9                      -- Radi
+@app.get("/boards/{board_id}")
+async def getBoardInfo(board_id):   
+    fetched_board_data = client.get(f"boards/{board_id}")
+    board = Board(
+        fetched_board_data.get("id"),
+        fetched_board_data.get("name"),
+        fetched_board_data.get("desc"),
+        fetched_board_data.get("shortUrl")
+    )
+    
+    return board
+
+#http://127.0.0.1:8000/cards/OidP4NJH                               -- Radi
+comments = []
+@app.get("/cards/{card_id}")
+async def getComment(card_id):
+    card_comments_data = client.get(f'cards/{card_id}/actions?filter=commentCard')
+    for attachment_data in card_comments_data:
+        comment = Comment(
+            attachment_data.get("id"),
+            attachment_data.get("idMemberCreator"),
+            attachment_data.get("date"),
+            attachment_data['data']['text']
+        )
+        comments.append(comment)
+    return comments 
+
+cards = []
+#127.0.0.1:8000/lists/66460051007109943d036a44                      -- Radi?
+@app.get("/lists/{list_id}")
+async def getListInfo(list_id):
+    fetched_card_data = client.get("boards/6645fee8d15bb6bc3076c8e9/cards")
+    for card in fetched_card_data:
+        cards.append(Card(card.get("id"), card.get("name"), card.get("desc"), card.get("shortUrl")))
+    return cards
+
+
+#http://127.0.0.1:8000/card/66460051007109943d036a44                -- Radi kroz docs
+@app.post("/card/{list_id}")
+async def napravi_karticu(list_id):
+     api_key = "737f990a50b95a1db675188c99175c8a"
+     token = "ATTA7d15664b7f73521ad1fafa12717b24d6de9136f780224304473d52fe59b0452e0935F6AF"
+     url_post = "https://api.trello.com/1/cards"
+     query_post = {"key":api_key,"token":token,"name":"nova kartica","desc":"deskripcija","idList":list_id}
+     response = requests.post(url_post,query_post,timeout=10)
+
+#http://127.0.0.1:8000/list/6645fee8d15bb6bc3076c8e9                -- Radi kroz docs
+@app.post("/list/{board_id}")
+async def nova_lista(board_id):
+    api_key = "737f990a50b95a1db675188c99175c8a"
+    token = "ATTA7d15664b7f73521ad1fafa12717b24d6de9136f780224304473d52fe59b0452e0935F6AF"
+    url_post = "https://api.trello.com/1/lists"
+    query_post = {"name":"NovaLista","idBoard":board_id,"key":api_key,"token":token}
+    response = requests.post(url_post,query_post,timeout=10)
+    
